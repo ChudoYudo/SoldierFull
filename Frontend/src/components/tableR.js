614bd15b55768582d0e6b554319561b9c50bcdce
Roman <raman.yudo@gmail.com>
2020-01-23 13:43:59 +0300

    add delete button

--- Frontend/src/components/tableR.js
+++ Frontend/src/components/tableR.js
@@ -1,30 +1,49 @@
 import React from 'react';
 import ModalExample from "./modal";
+import Button from "reactstrap/es/Button";
 
 
-const TableRow = ({soldier}) => {
-    return (
-        <tr>
+class TableRow extends React.Component{
 
-            <td className="pa3 bb b--black-10">
-                    {soldier.id}
-            </td>
-            <td className="pa3 bb b--black-10">
-                {soldier.first_name}
-            </td>
-            <td className="pa3 bb b--black-10">
-                {soldier.last_name}
-            </td>
-            <td className="pa3 bb b--black-10">
-                {soldier.third_name}
-            </td>
-            <td className="pa3 bb b--black-10">
-                {soldier.milU}
-            </td>
-            <td className="pa3 bb b--black-10" width={"5%"}>
-                <ModalExample buttonLabel={"edit"} soldierId={soldier.id}/>
-            </td>
-        </tr>
-    )
+    constructor(props) {
+        super(props);
+        this.state = {
+            kind: '',
+            data: []
+        };
+        this.del=this.del.bind(this);
+    }
+    del (e,id){
+        this.props.del(id);
+    }
+    render() {
+        return (
+            <tr>
+
+                <td className="pa3 bb b--black-10">
+                    {this.props.soldier.id}
+                </td>
+                <td className="pa3 bb b--black-10">
+                    {this.props.soldier.first_name}
+                </td>
+                <td className="pa3 bb b--black-10">
+                    {this.props.soldier.last_name}
+                </td>
+                <td className="pa3 bb b--black-10">
+                    {this.props.soldier.third_name}
+                </td>
+                <td className="pa3 bb b--black-10">
+                    {this.props.soldier.milU}
+                </td>
+                <td className="pa3 bb b--black-10" width={"5%"}>
+                    <ModalExample buttonLabel={"edit"} soldierId={this.props.soldier.id}/>
+                </td>
+                <td className="pa3 bb b--black-10" width={"5%"}>
+                    <Button onClick={(e)=>{this.del(e,this.props.soldier.id)}}> delete</Button>
+                </td>
+
+            </tr>
+        )
+    }
 }
 export default TableRow;
